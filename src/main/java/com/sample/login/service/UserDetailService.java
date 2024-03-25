8c3aa69fc893a589452ea5b65c696adb5372f9b9
wxodi0 <teayang06@gmail.com>
2024-03-25 23:30:23 +0900

    유저 DetailService 추가

---
+++ src/main/java/com/sample/login/service/UserDetailService.java
@@ -0,0 +1,17 @@
+package com.sample.login.service;
+import com.sample.login.domain.User;
+import com.sample.login.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailService implements UserDetailsService {
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) {
+    return userRepository.findByEmail(email).orElseThrow(() ->
+      new IllegalArgumentException("사용자를 찾을 수 없습니다.")
+    );
+  }
+}
